2248a0ea21e5d9fa71179a6705b48853cd7b8b10
jlamby <lamby.julien@gmail.com>
2017-05-30 12:18:59 +0200

    Mise à jour des dependances

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-var express 		= require('express');
+var express         = require('express');
+var logger          = require('morgan');
+var bodyParser      = require('body-parser');
 var expressResource = require('express-resource');
-var Config			= require('./lib/Config');
+var Config          = require('./lib/Config');
 
 var MessageEndpoint = require('./lib/endpoint/MessageEndpoint');
-var LinkEndpoint 	= require('./lib/endpoint/LinkEndpoint');
+var LinkEndpoint    = require('./lib/endpoint/LinkEndpoint');
 
 console.log("Server is starting ...");
 
 var app = express();
 
-app.use(express.logger());
-app.use(express.bodyParser());
+app.use(logger('tiny'));
+app.use(bodyParser.json());
 
 console.log("Creating routes ...");
 
@@ -18,7 +20,13 @@ app.resource('messages', MessageEndpoint);
 app.resource('links', LinkEndpoint);
 
 console.log("Routes : ");
-console.log(app.routes);
+app._router.stack.forEach(function(r){
+  if (r.route && r.route.path){
+    console.log(r.route.path)
+  }
+})
+
+
 
 app.listen(Config.SERVER_PORT);
 console.log("Node server listening on " + Config.SERVER_PORT);
